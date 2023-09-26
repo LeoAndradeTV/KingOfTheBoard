c2223925a8a0f203480fad0918218903acf99f5a
LeoAndradeTV <leocarlos.andrade@gmail.com>
2023-09-26 14:24:26 -0400

    KIN-11 Create Card SO
    Created scriptable object for cards so they can hold data

---
+++ Assets/Scripts/Cards/CardSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card SO", menuName = "Scriptable Objects/Card")]
+public class CardSO : ScriptableObject
+{
+    public string cardName;
+    public string cardDescription;
+    public int cardPrice;
+}
