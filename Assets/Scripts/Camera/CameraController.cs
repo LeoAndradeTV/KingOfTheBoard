0377c5dde87a268bb2092997c3390f4ffcf86a5a
LeoAndradeTV <leocarlos.andrade@gmail.com>
2023-10-03 16:01:12 -0400

    KIN-34 Can change camera view and move
    - Players can now view the table and move around it when in table view

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float moveSpeed = 20f;
+    private float zoomSpeed = 500f;
+
+    private float maxX = 5;
+    private float maxZ = 10;
+    private float minX = -5;
+    private float minZ = -10;
+    private float minZoom = 15;
+    private float maxZoom = 20;
+
+    private float xPosition;
+    private float zPosition;
+    private float zoom;
+
+    private void Start()
+    {
+        xPosition = transform.position.x;
+        zPosition = transform.position.z;
+        zoom = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ChangeCamera.CanMoveCamera)
+        {
+            return;
+        }
+
+        HandleCameraPosition();
+    }
+
+    private void HandleCameraPosition()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float zoomInput = Input.GetAxis("Mouse ScrollWheel");
+
+        xPosition += horizontalInput * moveSpeed * Time.deltaTime;
+        xPosition = Mathf.Clamp(xPosition, minX, maxX);
+
+        zPosition += verticalInput * moveSpeed * Time.deltaTime;
+        zPosition = Mathf.Clamp(zPosition, minZ, maxZ);
+
+        zoom -= zoomInput * zoomSpeed * Time.deltaTime;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+
+        transform.position = new Vector3(xPosition, zoom, zPosition);
+    }
+}
---
+++ Assets/Scripts/Camera/ChangeCamera.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeCamera : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Button changeCamButton;
+    [SerializeField] private Vector3 camOnBoardLocation;
+    [SerializeField] private Vector3 camOnTableLocation;
+    [SerializeField] private Vector3 camOnBoardRotation;
+    [SerializeField] private Vector3 camOnTableRotation;
+
+    public static bool CanMoveCamera {  get; private set; }
+
+    private bool isCamOnBoard = true;
+
+    private void Awake()
+    {
+        changeCamButton.onClick.RemoveAllListeners();
+        SetCamButton(true);
+    }
+
+    private async void LerpCamera(Vector3 location, Vector3 rotation, bool isCamOnBoard)
+    {
+        Quaternion finalRotation = Quaternion.Euler(rotation);
+        Vector3 startPos = cam.transform.position;
+        Quaternion startRot = cam.transform.localRotation;
+
+        this.isCamOnBoard = isCamOnBoard;
+        SetCamButton(this.isCamOnBoard);
+
+        float factor = 0;
+        while (factor < 1f)
+        {
+            cam.transform.position = Vector3.Lerp(startPos, location, factor);
+            cam.transform.localRotation = Quaternion.Lerp(startRot, finalRotation, factor);
+            factor += Time.deltaTime * 2;
+            await Task.Yield();
+        }
+
+        cam.transform.position = location;
+        cam.transform.localRotation = finalRotation;
+    }
+
+    private void SetCamButton(bool isCamOnBoard)
+    {
+        if (isCamOnBoard)
+        {
+            changeCamButton.onClick.RemoveAllListeners();
+            changeCamButton.onClick.AddListener(() => LerpCamera(camOnTableLocation, camOnTableRotation, false));
+            changeCamButton.GetComponentInChildren<TMP_Text>().text = "View Table";
+            CanMoveCamera = false;
+        } else
+        {
+            changeCamButton.onClick.RemoveAllListeners();
+            changeCamButton.onClick.AddListener(() => LerpCamera(camOnBoardLocation, camOnBoardRotation, true));
+            changeCamButton.GetComponentInChildren<TMP_Text>().text = "View Board";
+            CanMoveCamera = true;
+        }
+    }
+}
